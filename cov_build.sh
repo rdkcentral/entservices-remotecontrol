410125578dc931bda32ed7ea7029beee8d5ff89d
Michael Smith <michael_smith2@comcast.com>
2026-01-28 20:33:46 +0000

    Updates.

--- cov_build.sh
+++ cov_build.sh
@@ -54,6 +54,6 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-remotecontrol \
 -DUSE_DRM_SCREENCAPTURE -DHAS_API_SYSTEM -DHAS_API_POWERSTATE \
 -DHAS_RBUS -DDISABLE_SECURITY_TOKEN -DENABLE_DEVICE_MANUFACTURER_INFO -DUSE_THUNDER_R4=ON -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
-cmake --build build/entservices-remotecontrol --target install
+cmake --build build/entservices-remotecontrol/plugin --target install
 echo "======================================================================================"
 exit 0
