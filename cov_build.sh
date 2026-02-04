22eda027877eea3b9091d15e7661763fef79e3fa
Michael Smith <michael_smith2@comcast.com>
2026-02-04 15:29:59 +0000

    Testing ci.

--- cov_build.sh
+++ cov_build.sh
@@ -9,7 +9,7 @@ ls -la ${GITHUB_WORKSPACE}
 echo "building entservices-remotecontrol"
 
 cd ${GITHUB_WORKSPACE}
-cmake -G Ninja -S . -B build/entservices-remotecontrol \
+cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-remotecontrol \
 -DUSE_THUNDER_R4=ON \
 -DCMAKE_INSTALL_PREFIX="${GITHUB_WORKSPACE}/install/usr" \
 -DCMAKE_MODULE_PATH="${GITHUB_WORKSPACE}/install/tools/cmake" \
