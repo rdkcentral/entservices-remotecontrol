cf8f8fbc59ab158d312c2cf9ac9eea940a8018a1
Michael Smith <michael_smith2@comcast.com>
2026-02-06 18:03:04 +0000

    Testing test builds.

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -195,6 +195,7 @@ if $BUILD_TESTS; then
           -I $GITHUB_WORKSPACE/entservices-testframework/Tests/headers \
           -I /usr/include/gstreamer-1.0 \
           -I /usr/include/glib-2.0 \
+          -I /usr/lib/x86_64-linux-gnu/glib-2.0/include \
           -I $GITHUB_WORKSPACE/install/usr/include" \
 
     cmake --build build/mocks -j8
