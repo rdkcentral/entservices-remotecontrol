83936eeb01a22dd53c544257021f913d880795c1
Michael Smith <michael_smith2@comcast.com>
2026-09-23 15:41:21 +0000

    Fix

--- plugin/RemoteControl.h
+++ plugin/RemoteControl.h
@@ -88,11 +88,10 @@ namespace Plugin {
                 Exchange::JRemoteControl::Event::OnStatus(_parent, status);
             }
             void OnValidation(const Exchange::ValidationStatusObject& status) override {
-                LOGINFO("Notify onValidation netType=%u validationDigit1=%u validationDigit2=%u validationDigit3=%u",
-                    status.netType,
-                    status.validationDigit1,
-                    status.validationDigit2,
-                    status.validationDigit3);
+                LOGINFO("Notify onValidation status=%u code=%s key=%u",
+                    static_cast<unsigned>(status.status),
+                    status.code.c_str(),
+                    status.key);
                 Exchange::JRemoteControl::Event::OnValidation(_parent, status);
             }
             void OnFirmwareUpdateProgress(const Exchange::FirmwareUpdateStatusData& status) override {
